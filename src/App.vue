564a9208aea6f05b7e9b863d0505a47c41abb47e
Renaud Bresson <renaud.cci@gmail.com>
2023-12-20 11:07:31 +0100

    create/add-ContactForm

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,63 @@
 <template>
-  <AdventiceCard v-for="(adventice, key) in adventices" :key="key" :adventice="adventice" />
+  <div class="flex justify-center items-center">
+    <button class="text-white bg-neutral-500 px-6 py-2 rounded" @click="refreshPage">Réinitialiser</button>
+  </div>
+
+  <AdventiceCard v-for="(adventice, key) in adventicesArray" :key="key" :adventice="adventice" />
+
+  <div class="flex justify-center items-center mt-8">
+    <button class="text-white bg-neutral-500 px-6 py-2 rounded" @click="refreshPage">Réinitialiser</button>
+  </div>
+
+  <div class="flex justify-center items-center">
+    <ContactForm />
+  </div>
+
+  
+
 </template>
 
 <script>
-import AdventiceCard from './components/AdventiceCard.vue'
+import AdventiceCard from './components/AdventiceCard.vue';
+import ContactForm from './components/ContactForm.vue';
 import adventices from './assets/adventices.json';
 
 export default {
   name: 'App',
   components: {
     AdventiceCard,
+    ContactForm,
   },
-  data() {
-  return {
-    adventices
-  };
+   data() {
+    return {
+      adventicesArray: this.shuffle(Object.values(adventices))
+    };
+  },
+  methods: {
+    shuffle(array) {
+      let currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    },
+    refreshPage() {
+      window.location.reload();
+    },
+  }
 }
-}
+
 </script>
 
 <style scoped>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,53 @@
+<template>
+
+<div class="mt-16 w-5/6 sm:w-full flex flex-col justify-center">
+  <h3 class="text-2xl text-red-800 text-center">
+    <p>Un soucis d'image ?</p>
+    <p>Une erreur ?</p>
+    <p>Contactez-nous !</p>
+  </h3>
+
+  <div class="flex flex-col justify-center items-center text-center">
+      
+    <form
+      name="simple-contact-form"
+      action="https://formspree.io/f/xjvnbogr"
+      method="POST"
+      class="flex flex-col justify-center items-center"
+    >
+      <div class="flex justify-center items-center grid grid-cols-1 sm:grid-cols-2 mt-4">
+        <p class="col-span-1 text-sm sm:text-base">Nom complet :</p>
+        <input class="col-span-1 border border-neutral-200 rounded p-1" type="text" name="name" id="full-name" placeholder="Nom Prénom" required>
+      </div>
+
+      <div class="flex justify-center items-center grid grid-cols-1 sm:grid-cols-2 mt-4">
+        <p class="col-span-1 text-sm sm:text-base">Votre email :</p>
+        <input class="col-span-1 border border-neutral-200 rounded p-1" type="email" name="email" placeholder="adresse@exemple.com" required>
+      </div>
+
+      <div class="flex justify-center items-center grid grid-cols-1 mt-4 w-full">
+        <p class="col-span-1 text-sm sm:text-base">Votre message :</p>
+        <textarea class="col-span-1 border border-neutral-200 rounded p-1" rows="5" name="message" placeholder="Votre message ici..." required></textarea>
+      </div>
+      <!-- your other form fields go here -->
+      <button class="text-white bg-neutral-800 px-6 py-2 rounded my-4" type="submit">Envoyer</button>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ContactForm',
+  props: {
+  },
+  data() {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
