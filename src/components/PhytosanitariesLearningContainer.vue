4913a6cb45ce52059bb95e83fd176656a5fe54ce
Renaud Bresson <renaud.cci@gmail.com>
2024-01-16 11:33:18 +0100

    update/add-Phytosanitaries-button

---
+++ src/components/PhytosanitariesLearningContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+
+    <h1>Phytosanitaires</h1>
+    <h1>Phytosanitaires</h1>
+    <h1>Phytosanitaires</h1>
+
+  </section>
+
+  
+
+</template>
+
+<script>
+
+export default {
+  name: 'PhytosanitariesLearningContainer',
+  components: {
+  },
+   data() {
+    return {
+    };
+  },
+  methods: {
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
